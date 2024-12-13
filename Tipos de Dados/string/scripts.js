13b306bd6ab4b6e17e8183ecc40e1efd57e79177
LeoMedeirosss <lmf@cesar.school>
2024-12-13 19:01:24 -0300

    basic - strings

---
+++ Tipos de Dados/string/scripts.js
@@ -0,0 +1,13 @@
+console.log("tanto aspas simples, quanto aspas duplas, quanto crase funcionam")
+console.log('aspas simples')
+console.log("aspas duplas")
+console.log(`crase`)
+
+console.log("o barra n pula uma linha")
+console.log("uma linha \noutra linha")
+
+console.log("usando template literals")
+console.log(`2+2 é igual a ${2+2}`)
+
+console.log("somar ou concatenar strings")
+console.log("pedra " + "papel " + "tesoura")
