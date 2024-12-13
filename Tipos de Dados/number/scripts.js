2466b9bc167c0ba8d6cedce2529e4af6b4b52dfc
LeoMedeirosss <lmf@cesar.school>
2024-12-13 18:45:07 -0300

    basic - numbers

---
+++ Tipos de Dados/number/scripts.js
@@ -0,0 +1,27 @@
+console.log("Numeros");
+console.log(5);
+console.log(1.3);
+console.log(-29);
+
+console.log("Demonstrando o tipo");
+console.log(typeof 5);
+console.log(typeof 1.3);
+console.log(typeof -29);
+
+console.log("Operações");
+console.log(2-1);
+console.log(3*5);
+console.log(10+10);
+console.log(6/2);
+console.log(20%2);
+console.log(21%2);
+
+console.log("números especiais");
+console.log(Infinity);
+console.log(-Infinity);
+console.log(NaN);
+
+console.log("tipos dos números especiais");
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(typeof NaN);
