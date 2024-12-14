6fc4f5b269cfdad225f281e69d9f561800e55e24
LeoMedeirosss <lmf@cesar.school>
2024-12-14 17:19:16 -0300

    basic- exercicios

---
+++ Tipos de Dados/exercising/scripts.js
@@ -0,0 +1,24 @@
+console.log(1)
+console.log('aspas')
+console.log("aspas duplas")
+console.log(`trema`)
+
+console.log(2)
+console.log(5)
+console.log(10.7)
+console.log(10-44)
+
+console.log(3)
+console.log(6 > 4)
+console.log(7 <= 10)
+console.log(5 == "5")
+console.log(3 != 8)
+
+console.log(4)
+console.log(6 > 3 && 4 > 2)
+console.log(10 < 2 || 0 == 0)
+console.log(!(true && false))
+
+console.log(5)
+console.log("string" * 1.5)
+
