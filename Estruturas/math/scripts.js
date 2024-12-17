74236dcfeacbfc0ad8ba5b712291455aa9e4abad
LeoMedeirosss <lmf@cesar.school>
2024-12-17 17:28:13 -0300

    estrtura - math.

---
+++ Estruturas/math/scripts.js
@@ -0,0 +1,11 @@
+let maiorNumero = Math.max(1,5,2,4)
+
+console.log(maiorNumero)
+
+let menorNumero = Math.min(9,6,3,4)
+
+console.log(menorNumero)
+
+let redondo = Math.round(10.8493857311)
+
+console.log(redondo)
