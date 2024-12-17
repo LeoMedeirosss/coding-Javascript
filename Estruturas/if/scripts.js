770bb3d006dbc6dd510d276d77199cbc689e9474
LeoMedeirosss <lmf@cesar.school>
2024-12-17 17:48:34 -0300

    estruturas - ifelse

---
+++ Estruturas/if/scripts.js
@@ -0,0 +1,18 @@
+let idade = 18
+
+let nome = "João"
+
+passaporte = true
+
+if(idade >= 18) {
+    console.log("pode entrar")
+} else {
+    console.log("não pode entrar")
+}
+
+
+if(nome == "João" && idade == 20 || passaporte == true) {
+    console.log("Liberado")
+} else{
+    console.log("Não tá liberado")
+}
