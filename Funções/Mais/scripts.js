c3b7518e2578a42d1174daef0c6b07f1c995ab8b
LeoMedeirosss <lmf@cesar.school>
2024-12-20 17:55:12 -0300

    funções - definição

---
+++ Funções/Mais/scripts.js
@@ -0,0 +1,21 @@
+function imprimirNoConsole(){
+    console.log("Olá Mundo")
+}
+
+imprimirNoConsole();
+
+function imprimirNum(num) {
+    console.log("O numero é: " + num)
+}
+
+ImprimirNum(3)
+ImprimirNum(10)
+ImprimirNum(305812)
+
+const numAleatorio = function() {
+    console.log(Math.random())
+}
+
+numAleatorio()
+numAleatorio()
+
