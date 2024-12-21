14c5f137ff5cf405153c555fa6991b372f389878
LeoMedeirosss <lmf@cesar.school>
2024-12-21 01:22:44 -0300

    funções - mais

--- Funções/Mais/scripts.js
+++ Funções/Mais/scripts.js
@@ -1,21 +1,18 @@
-function imprimirNoConsole(){
-    console.log("Olá Mundo")
+function multiplicar(x,y,z){
+    return x*y*z;
 }
 
-imprimirNoConsole();
+console.log(multiplicar(2,4,2))
 
-function imprimirNum(num) {
-    console.log("O numero é: " + num)
+function dirigir(idade,cnh){
+    if(idde >= 18 && cnh == true){
+        console.log("pode dirigir")
+    } else {
+        console.log("Não pode dirigir")
+    }
 }
 
-ImprimirNum(3)
-ImprimirNum(10)
-ImprimirNum(305812)
-
-const numAleatorio = function() {
-    console.log(Math.random())
-}
-
-numAleatorio()
-numAleatorio()
-
+console.log(dirigir(19,true))
+console.log(dirigir(19,false))
+console.log(dirigir(17,true))
+console.log(dirigir(17,false))
