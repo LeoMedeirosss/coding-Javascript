e5d4b08fbd428d6c29cdce2245afe8fc7e853f3a
LeoMedeirosss <lmf@cesar.school>
2024-12-21 01:28:46 -0300

    funções - escopo

---
+++ Funções/Escopo/scripts.js
@@ -0,0 +1,21 @@
+let y = 10
+
+function imprimir(){
+    let y = 11
+
+    console.log(y)
+}
+
+imprimir()
+
+console.log(y)
+
+
+let x = 10
+
+if(x>5){
+    let x = 20
+    console.log(x)
+}
+
+console.log(x)
