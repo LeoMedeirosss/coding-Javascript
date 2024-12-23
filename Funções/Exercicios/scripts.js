b268abd86ffcda8e997bcd1486b13fa101639791
LeoMedeirosss <lmf@cesar.school>
2024-12-23 15:59:46 -0300

    funções - exercicios 1.0

---
+++ Funções/Exercicios/scripts.js
@@ -0,0 +1,29 @@
+console.log(1)
+function pontoLog(){
+    console.log("Texto")
+}
+pontoLog()
+
+console.log(" ")
+console.log(2)
+function temIdade(idade){
+    console.log(`você tem ${idade} anos`)
+}
+temIdade(20)
+
+console.log(" ")
+console.log(3)
+function somar(x,y){
+   return x + y
+}
+console.log(somar(10,2))
+
+console.log(" ")
+console.log(4)
+function random(x){
+    return Math.floor(Math.random() * x) + 1
+}
+console.log(random(100))
+
+console.log(" ")
+console.log(5)
