8ab6799240f3a99fcdceeef1ad3c29625b19b428
LeoMedeirosss <lmf@cesar.school>
2024-12-26 15:16:06 -0300

    arrays - objetos

--- Arrays e Objetos/Objetos/scripts.js
+++ Arrays e Objetos/Objetos/scripts.js
@@ -1,3 +1,32 @@
-let alerta = alert("ALERTA")
+let obj = {
+    nome:"João",
+    profissao: "Engenheiro",
+    idade: 20,
+    strings: function(){
+        console.log("abc")
+    }
+}
 
-let alerta2 = alert("ALERTA2")
+let obj2 = {
+    prop: "propriedade"
+}
+
+console.log(obj.nome)
+console.log(obj.idade)
+obj.strings
+
+delete obj.profissao
+console.log(obj.profissao)
+
+obj.booleano = true
+console.log(obj.booleano)
+
+Object.assign(obj, obj2)
+console.log(obj)
+console.log(obj2)
+
+let obj3 = {
+    prop1: "teste",
+    prop2: "teste2"
+}
+console.log(Object.keys(obj3))
