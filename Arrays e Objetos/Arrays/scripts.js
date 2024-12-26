e20133f0b264e07b29f2212508ec0f8d115c3f82
LeoMedeirosss <lmf@cesar.school>
2024-12-26 15:27:36 -0300

    arrays e obj - array loop

--- Arrays e Objetos/Arrays/scripts.js
+++ Arrays e Objetos/Arrays/scripts.js
@@ -1,6 +1,10 @@
-let arrayNum = [1,2,3,4,5]
+let arrayNum = [1,2,3,4,5,6,7,8,9,10]
 let arrayNome = ["nome1", "nome2","nome3", "nome4"]
 let arrayBool = [true, false, true, true]
 
 console.log(arrayNum[0], arrayNome[3], arrayBool[1])
 console.log(arrayBool.length)
+
+for (let i = 0; i < arrayNum.length; i++) {
+    console.log(arrayNum[i])
+}
