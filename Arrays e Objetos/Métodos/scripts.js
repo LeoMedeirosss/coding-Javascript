22784c0d7f36be22abe12698ca73e3ef43f6ee21
LeoMedeirosss <lmf@cesar.school>
2024-12-26 17:58:24 -0300

    arrays e obj - pop e push

--- Arrays e Objetos/Métodos/scripts.js
+++ Arrays e Objetos/Métodos/scripts.js
@@ -10,3 +10,12 @@ console.log(typeof nome.toUpperCase) //function
 
 //string.propriedade
 //string.metodo()
+
+
+//MÉTODOS
+
+let array = [1,2,3,4,5,6,7,8,9,10]
+
+let pop = array.pop() //remover o último elemento
+
+let push = array.push(10)
