c527e3853e65395289c8051dfcf756227c4bb021
LeoMedeirosss <lmf@cesar.school>
2024-12-26 14:45:13 -0300

    arrays e obj - arrays

--- Arrays e Objetos/Arrays/scripts.js
+++ Arrays e Objetos/Arrays/scripts.js
@@ -1,3 +1,6 @@
-let alerta = alert("ALERTA")
+let arrayNum = [1,2,3,4,5]
+let arrayNome = ["nome1", "nome2","nome3", "nome4"]
+let arrayBool = [true, false, true, true]
 
-let alerta2 = alert("ALERTA2")
+console.log(arrayNum[0], arrayNome[3], arrayBool[1])
+console.log(arrayBool.length)
