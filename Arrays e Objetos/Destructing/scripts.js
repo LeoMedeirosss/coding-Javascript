2ab63b4877041ce8a1eab795d9a3d703cc306fa1
LeoMedeirosss <lmf@cesar.school>
2024-12-28 20:24:50 -0300

    arrays e obj - destructing

--- Arrays e Objetos/Destructing/scripts.js
+++ Arrays e Objetos/Destructing/scripts.js
@@ -1,3 +1,11 @@
-let alerta = alert("ALERTA")
+let obj = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4
+}
 
-let alerta2 = alert("ALERTA2")
+const {a:vA, b: vB, c: vC, d:vD} = obj
+
+console.log(vA)
+console.log(vD)
