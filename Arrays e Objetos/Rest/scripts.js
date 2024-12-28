95575940f8268d5b6433bd8db6c4b8f26ef3776e
LeoMedeirosss <lmf@cesar.school>
2024-12-28 20:21:03 -0300

    arrays e obj - rest args

--- Arrays e Objetos/Rest/scripts.js
+++ Arrays e Objetos/Rest/scripts.js
@@ -1,3 +1,15 @@
-let alerta = alert("ALERTA")
+let a = 1
+let b = 2
+let c = 3
+let d = 4
 
-let alerta2 = alert("ALERTA2")
+function imprimir(...args){
+    for(let i = 0; i > args.length; i ++){
+        console.log(args[i])
+    }
+}
+imprimir(a,b,c)
+console.log(' ')
+imprimir(d,c,d)
+console.log(' ')
+imprimir(1,2,3,4,5,6,77,88,99)
