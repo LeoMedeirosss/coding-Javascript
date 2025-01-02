c432c7a76aed4e2be2fc312b9260c65758359407
LeoMedeirosss <lmf@cesar.school>
2025-01-02 16:57:21 -0300

    OO - Prototype

--- Conceitos de OO/Prototypes/scripts.js
+++ Conceitos de OO/Prototypes/scripts.js
@@ -1,11 +1,17 @@
-console.log("valores booleanos")
-console.log(true)
-console.log(false)
-console.log(5>2)
-console.log(2>5)
-console.log(2<=2)
-console.log(3>=6)
-console.log("2" == 2)
-console.log(2 === "2")
-console.log(2 === 2)
+//objeto fallback de outro objeto(fallback = filho)
+//quando um objeto recebe uma requisição de uma propriedade que não tem
+//ela procura no prototype deste objeto.
 
+const obj = {
+    numero: 10,
+}
+
+console.log(Object.getPrototypeOf(obj))
+console.log(Object.getPrototypeOf(obj) === Object.prototype)
+console.log(obj.hasOwnProperty('numero')) //true
+console.log(obj.hasOwnProperty('cor')) // false
+
+const obj2 = Object.create(obj)
+console.log(obj2.numero)
+console.log(obj2.hasOwnProperty('numero')) //false, pois quem tem a propriedade é obj, 
+                                                        //e não obj2    
