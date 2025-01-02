d5679be151bc153fb85542ca6449f480d559e5fb
LeoMedeirosss <lmf@cesar.school>
2025-01-02 17:23:04 -0300

    OO - classes 1.0

--- Conceitos de OO/Classes/scripts.js
+++ Conceitos de OO/Classes/scripts.js
@@ -1,11 +1,16 @@
-console.log("valores booleanos")
-console.log(true)
-console.log(false)
-console.log(5>2)
-console.log(2>5)
-console.log(2<=2)
-console.log(3>=6)
-console.log("2" == 2)
-console.log(2 === "2")
-console.log(2 === 2)
+//classe é um molde de um ou mais objetos
+// é o prototype do javascript
 
+let cachorro = {
+    patas: 4,
+    raça: " ",
+    latir: function(){
+        console.log("ruf ruf")
+    }
+}
+
+let labrador = Object.create(cachorro)
+let yorkshire = Object.create(cachorro)
+
+labrador.raça = "Labrador"
+yorkshire.raça = "York"
