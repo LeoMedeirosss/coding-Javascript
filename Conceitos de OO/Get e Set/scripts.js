0607a4fa402b1106ca939f24b5cff94af120a357
LeoMedeirosss <lmf@cesar.school>
2025-01-03 17:06:23 -0300

    OO - get e set

--- Conceitos de OO/Get e Set/scripts.js
+++ Conceitos de OO/Get e Set/scripts.js
@@ -1,11 +1,28 @@
-console.log("valores booleanos")
-console.log(true)
-console.log(false)
-console.log(5>2)
-console.log(2>5)
-console.log(2<=2)
-console.log(3>=6)
-console.log("2" == 2)
-console.log(2 === "2")
-console.log(2 === 2)
+//muito comum em linguagens OO, como java
 
+class pessoa{
+    constructor(nome, altura){
+        this.nome = nome
+        this.altura = altura
+    }
+
+    get getNome(){
+        return this.nome
+    }
+    get getAltura(){
+        return this.altura
+    }
+
+    set setNome(nome){
+        this.nome = nome
+    }
+    set setAltura(altura){
+        this.altura = altura
+    }
+}
+
+pessoa.setNome = "João"
+pessoa.setAltura = 1.75
+
+console.log(pessoa.getNome)
+console.log(pessoa.getAltura)
