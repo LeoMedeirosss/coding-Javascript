94009406beafcfe54ea042731cfbe497fc905cc2
LeoMedeirosss <lmf@cesar.school>
2025-01-07 17:07:49 -0300

    Erros - use strict

--- Debug e Erros/Strict Mode/scripts.js
+++ Debug e Erros/Strict Mode/scripts.js
@@ -1,49 +1,12 @@
-//classe é um molde de um ou mais objetos
-// é o prototype do javascript
+"use strict"
 
-let cachorro = {
-    patas: 4,
-    raça: " ",
-    latir: function(){
-        console.log("ruf ruf")
-    }
+variavel = "teste" //erro. Se, o "use strict", não daria erro
+
+let variavel2 = "teste" //sem erro
+
+function teste(){
+    "use strict"
+    t = 5 //erro
+    let a = 3
 }
-
-let labrador = Object.create(cachorro)
-let yorkshire = Object.create(cachorro)
-
-labrador.raça = "Labrador"
-yorkshire.raça = "York"
-
-//duas formas de instanciar um objeto de uma classe
-
-function criarCachorro(raça,patas,cor) {
-    let cachorro = Object.create({})
-    cachorro.raça = raça
-    cachorro.patas = patas
-    cachorro.cor = cor
-    return cachorro
-}
-
-let doberman = criarCachorro("doberman",4,"preto")
-console.log(doberman)
-
-function criarCachorro2(raça,patas,cor){
-    this.raça = raça
-    this.patas = patas
-    this.cor = cor
-}
-
-let husky = new criarCachorro2("Husky",4,"branco")
-
-console.log(husky)
-
-class dog{
-    constructor(raca){
-        this.raca = raca
-    }
-}
-let patas = Symbol()
-dog.prototype[patas] = 4
-let golden = new dog("Golden Retriever")
-console.log(dog.prototype[patas])
+teste()
