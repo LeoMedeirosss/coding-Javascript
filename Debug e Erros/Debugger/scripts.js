49f6c8ea816365448bd9152ecc1f52957ed3f5b1
LeoMedeirosss <lmf@cesar.school>
2025-01-07 17:29:05 -0300

    Erros- Debugger

---
+++ Debug e Erros/Debugger/scripts.js
@@ -0,0 +1,22 @@
+let a = 1
+let b = 2
+let c = 3
+
+if(c > a){
+    a-b
+    debugger;
+}
+
+if(b == a){
+    a++
+} else{
+    a = a+b+c
+}
+
+debugger;
+
+a = a * c
+
+debugger;
+
+//pode se usar o console.log() também.
