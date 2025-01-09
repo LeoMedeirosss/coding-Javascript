c38e7a475f6d9b522ae84056a74a4966d5f4013e
LeoMedeirosss <lmf@cesar.school>
2025-01-09 17:12:48 -0300

    Erros - Assertion Final

--- Debug e Erros/Assertion/scripts.js
+++ Debug e Erros/Assertion/scripts.js
@@ -1,49 +1,29 @@
-//classe é um molde de um ou mais objetos
-// é o prototype do javascript
+//verificações no programa, que são utilizadas para ver que tudo vai 
+//ocorrer da forma esperada.
 
-let cachorro = {
-    patas: 4,
-    raça: " ",
-    latir: function(){
-        console.log("ruf ruf")
+let arr = [1,2,3,4,5]
+let arr2 = []
+
+function iterarArray(arr){
+    if(arr.length == 0){
+        throw new Error("o array precisa ter ao menos 1 elemento.")
+    } else {
+        for(let i = 0;i < arr.length;i++ ){
+            console.log(i)
+        }
     }
 }
 
-let labrador = Object.create(cachorro)
-let yorkshire = Object.create(cachorro)
-
-labrador.raça = "Labrador"
-yorkshire.raça = "York"
-
-//duas formas de instanciar um objeto de uma classe
-
-function criarCachorro(raça,patas,cor) {
-    let cachorro = Object.create({})
-    cachorro.raça = raça
-    cachorro.patas = patas
-    cachorro.cor = cor
-    return cachorro
-}
-
-let doberman = criarCachorro("doberman",4,"preto")
-console.log(doberman)
-
-function criarCachorro2(raça,patas,cor){
-    this.raça = raça
-    this.patas = patas
-    this.cor = cor
-}
-
-let husky = new criarCachorro2("Husky",4,"branco")
-
-console.log(husky)
-
-class dog{
-    constructor(raca){
-        this.raca = raca
+function vazioArray(arr){
+    if(arr.length > 0){
+        throw new Error("o array não está vazio")
+    } else {
+        console.log("o array está vazio")
     }
 }
-let patas = Symbol()
-dog.prototype[patas] = 4
-let golden = new dog("Golden Retriever")
-console.log(dog.prototype[patas])
+
+iterarArray(arr)
+iterarArray(arr2)
+
+vazioArray(arr)
+vazioArray(arr2)
