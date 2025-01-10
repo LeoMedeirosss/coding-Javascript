57438c483d6b4d62429baaf9114191e59d8bf2f0
LeoMedeirosss <lmf@cesar.school>
2025-01-10 15:44:39 -0300

    Regex - conjuntos

--- Regex/Conjuntos/scripts.js
+++ Regex/Conjuntos/scripts.js
@@ -1,7 +1,15 @@
-for (let x = 0; x < 30; x++) {
-    if(x == 10){
-        console.log("é igual a 10")
-        break
-    }
-    console.log("loopando " + x)
-}
+//é possível definir um conjunto de caracteres para encontrar por regex
+
+const reg1 = /[12345]/ //se qualquer um desses caracteres estiver presente na string, retornará true
+
+console.log(reg1.test("numero 4")) //true
+console.log(reg1.test("numero 77")) //false
+console.log(reg1.test("numero 123")) //true
+
+const reg2 = /[0-9]/ //se qualquer número nesse intervalo de 0-9 estiver na string, retornará true
+//reg2 basicamente verifíca se existe um número na string
+console.log(reg2.test("68")) //true
+console.log(reg2.test("102948145")) //true
+
+
+
