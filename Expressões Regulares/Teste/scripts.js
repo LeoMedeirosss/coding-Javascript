517a2d922e8384f298b5659bd0671b35ca35eafb
LeoMedeirosss <lmf@cesar.school>
2025-01-10 15:29:21 -0300

    Regex - Arquivos

--- Expressões Regulares/Teste/scripts.js
+++
@@ -1,7 +0,0 @@
-for (let x = 0; x < 30; x++) {
-    if(x == 10){
-        console.log("é igual a 10")
-        break
-    }
-    console.log("loopando " + x)
-}
---
+++ Regex/Teste/scripts.js
@@ -0,0 +1,3 @@
+//forma de encontrar padrões em uma string.
+//ex: validar se só tem números.
+
