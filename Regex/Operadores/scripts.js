71d811c6d59c9b2af34edbd767a1dceae0310815
LeoMedeirosss <lmf@cesar.school>
2025-01-12 17:15:18 -0300

    Regex - Operadores Not, Plus e Question

--- Regex/Operadores/scripts.js
+++ Regex/Operadores/scripts.js
@@ -1,7 +1,26 @@
-for (let x = 0; x < 30; x++) {
-    if(x == 10){
-        console.log("é igual a 10")
-        break
-    }
-    console.log("loopando " + x)
-}
+//Operadores
+
+//Not -> ^
+const semAeB = /[^ab]/
+console.log(semAeB.test("aaaa")) //false
+console.log(semAeB.test("aabbbbabab")) //false
+console.log(semAeB.test("aaaca")) //true, pois tem o "C" também.
+
+const semAateZ = /[^a-z]/
+console.log(semAateZ.test("abcdemioklpppp")) //false
+console.log(semAateZ.test("abcdemioklppppç")) //true, pois tem o "Ç" também.
+console.log(semAateZ.test("abcdefghijk9")) //true, pois tem o "9" também.
+
+//Plus -> +
+const umOuMaisNum = /\d+/ //qualquer quantidade de números.
+console.log(umOuMaisNum.test("19048u938106478311")) //true
+console.log(umOuMaisNum.test(" ")) //false
+console.log(umOuMaisNum.test("acbd123564444")) //true
+
+//Question -> ?
+const opc = /abcde?/ //o "e" é opcional.
+console.log(opc.test("abcde")) //true
+console.log(opc.test("abcd")) //true
+console.log(opc.test("ae")) //false
+console.log(opc.test("abcdefg")) //true
+console.log(opc.test("abcdfg")) //true
