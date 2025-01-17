95e142770355493a2dd64764ffcfd29ff56d660e
LeoMedeirosss <lmf@cesar.school>
2025-01-17 16:18:00 -0300

    Prog. Assincrona - Callback

--- Prog. Assincrona/Callbacks/scripts.js
+++ Prog. Assincrona/Callbacks/scripts.js
@@ -1,3 +1,13 @@
-let alerta = alert("ALERTA")
+//função que faz uma ação depois de algum acontecimento no código.
 
-let alerta2 = alert("ALERTA2")
+console.log("1")
+
+setTimeout(function() {
+    console.log("5") 
+}, 3000 ) //3 segundos depois de ser executado.
+
+console.log("2")
+console.log("3")
+console.log("4")
+
+//no console, os números ficam em ordem:1, 2, 3, 4, 5
