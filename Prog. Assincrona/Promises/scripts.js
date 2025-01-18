8f22693e5a9ecb48f8ca68dccad4f7145f04162b
LeoMedeirosss <lmf@cesar.school>
2025-01-18 17:57:19 -0300

    Prog. Assincrona - Promises Error

--- Prog. Assincrona/Promises/scripts.js
+++ Prog. Assincrona/Promises/scripts.js
@@ -7,6 +7,15 @@ console.log("Outros códigos")
 console.log(promessa)
 
 promessa.then((value) => (console.log(`o valor é ${value}`)))
+        .then((value) => {return value + 5})
+        .then((value) => (console.log(value)))
 
 //falha nas promises
 
+console.log(" ")
+let promessa2 = Promise.resolve(new Error("falha"))
+
+console.log("Outros códigos")
+
+promessa2.then(value=> console.log(value))
+         .catch(reason => console.log("Falhou " + reason))
