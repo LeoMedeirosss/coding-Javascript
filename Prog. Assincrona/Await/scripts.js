568c7deaff124bd3ac27a2076ba56c44474eb18b
LeoMedeirosss <lmf@cesar.school>
2025-01-18 18:13:57 -0300

    Prog Assincrona - Await

--- Prog. Assincrona/Await/scripts.js
+++ Prog. Assincrona/Await/scripts.js
@@ -1,3 +1,16 @@
-let alerta = alert("ALERTA")
+function somaDelay(a,b){
+    return new Promise(resolve => {
+        setTimeout(function(){
+            resolve(a+b)
+        }, 4000 /* 4 segundos */ )
+    })
+}
 
-let alerta2 = alert("ALERTA2")
+async function resSoma(a,b,c) {
+    let x = somaDelay(a,b)
+    let y = c
+
+    return await x + y
+}
+
+resSoma(1,2,3).then(value => console.log(value))
