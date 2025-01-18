307994306c40398b7207c5d12af27ad507bc8dec
LeoMedeirosss <lmf@cesar.school>
2025-01-18 18:29:53 -0300

    Prog Assincrona - Generators

--- Prog. Assincrona/Generators/scripts.js
+++ Prog. Assincrona/Generators/scripts.js
@@ -1,3 +1,16 @@
-let alerta = alert("ALERTA")
+//Generators são semelhantes as Promises
+//ações podem ser pausadas e continuadas depois
 
-let alerta2 = alert("ALERTA2")
+function* criadorID(){
+    let id = 0
+    while(true) {
+        yield id++
+    }
+}
+
+let criarID = criadorID()
+
+console.log(criarID.next().value)
+console.log(criarID.next().value)
+console.log(criarID.next().value)
+console.log(criarID.next().value)
