01b0cbe53eefc56ded1c5d031672a18dda998b3e
LeoMedeirosss <lmf@cesar.school>
2025-01-20 17:44:20 -0300

    WEB - DOM

---
+++ Web/DOM/scripts.js
@@ -0,0 +1,11 @@
+//quanto uma página web é requisitada, ela recebe um texto que é transformado
+//em uma estrutura html
+
+//DOM: document object model
+//Por meio do Javascript é possivel acessar cada um dos elementos do html pelo DOM.
+
+//É possível mudar completamente uma página html através do DOM(elementos,atributos,css...)
+//Alterar, adicionar e remover.
+
+//É possivel acessar e visualizar o documento do DOM pelo console.
+//console.log(document.body) - por exemplo.
