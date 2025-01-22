8606dde6a8ab849be387b689fcba7dd6eb7769b8
LeoMedeirosss <lmf@cesar.school>
2025-01-22 16:36:40 -0300

    WEB - InsertBefore e appendChild

---
+++ Web/Alterar HTML Dom/scripts.js
@@ -0,0 +1,20 @@
+//Alterar DOM com insertBefore
+
+let novoElemento = document.createElement("p")
+let elementoAlvo = document.querySelector("#principal")
+let elementoPai = document.querySelector("#conteiner")
+
+//o insertbefore precisa de 2 parâmetros.
+//o novo elemento a ser inserido e o elemento alvo, onde ele vai ser inserido.
+elementoPai.insertBefore(novoElemento, elementoAlvo)
+
+//Alterar DOM com appendChild
+//appendChild insere o elemento após todos os elementos do elemento pai.
+
+let novoElemento2 = document.createElement("p")
+let texto = document.createTextNode("Algum Texto")
+novoElemento2.appendChild(texto)
+
+let p = document.querySelector("#texto1")
+let pai = p.parentNode
+pai.appendChild(novoElemento2)
