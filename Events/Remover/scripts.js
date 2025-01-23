f3f01fe44b41cdd33260cb0b262fde15eff037f9
LeoMedeirosss <lmf@cesar.school>
2025-01-23 16:15:17 -0300

    Events - Remover Evento

--- Events/Remover/scripts.js
+++ Events/Remover/scripts.js
@@ -1,3 +1,15 @@
-let alerta = alert("ALERTA")
+//É possível remover eventos quando for necessário.
+//removeEventListener, passando o evento e a função que o evento está escutando.
 
-let alerta2 = alert("ALERTA2")
+let btn = document.getElementById("button1")
+let btn2 = document.getElementById("button2")
+
+function mensagem() {
+    console.log("Mensagem enviada")
+}
+
+btn.addEventListener("click", mensagem)
+
+btn2.addEventListener("click", function(){
+    btn.removeEventListener("click",mensagem)
+})
