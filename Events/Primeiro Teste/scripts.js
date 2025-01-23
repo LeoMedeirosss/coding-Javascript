298992748664fa37521d8717610a2b6ce10aea23
LeoMedeirosss <lmf@cesar.school>
2025-01-23 16:00:29 -0300

    Eventos - primeiros testes

--- Events/Primeiro Teste/scripts.js
+++ Events/Primeiro Teste/scripts.js
@@ -1,3 +1,8 @@
-let alerta = alert("ALERTA")
+//Eventos são ações condicionadas a algum tipo de resposta feita pelo usuário
+//É possível atrelar lógica a essas ações do usuário
+//Clicks, movimento do mouse, etc.
 
-let alerta2 = alert("ALERTA2")
+let btn = document.getElementById("button1")
+btn.addEventListener("click", function() {
+    console.log("apertou")
+})
