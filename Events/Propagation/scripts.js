bccd8e3bba90ba3690b437a5100d68377c5e2c6b
LeoMedeirosss <lmf@cesar.school>
2025-01-24 15:35:12 -0300

    Eventos - stopPropagation

--- Events/Propagation/scripts.js
+++ Events/Propagation/scripts.js
@@ -1,3 +1,17 @@
-let alerta = alert("ALERTA")
+//Quando um elemento não é bem definido ou um elemento que está dentro de outro evento.
+//Pode acontecer a propagação, ou seja, o outro elemento ativar um evento
+//Aconteceria uma duplicação.
+//Método stopPropagation.
 
-let alerta2 = alert("ALERTA2")
+let btn = document.getElementById("button")
+let p = document.getElementById("capsula")
+
+function msg(e){
+    console.log("clicou no button")
+    e.stopPropagation();
+}
+
+btn.addEventListener("click",msg)
+p.addEventListener("click",function(){
+    console.log("clicou no parágrafo")
+})
