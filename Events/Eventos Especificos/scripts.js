c2c85e8682f4676b8072204f2644a4e0b391ada0
LeoMedeirosss <lmf@cesar.school>
2025-01-24 18:14:18 -0300

    Eventos - Mouse 1.0

--- Events/Eventos Especificos/scripts.js
+++ Events/Eventos Especificos/scripts.js
@@ -14,3 +14,34 @@ window.addEventListener('keyup',function(e){
 })
 
 //Eventos de Mouse
+//mousedown e mouseup
+//dblclick
+//botão direito no btn2
+
+let btn1 = document.getElementById("button1")
+let btn2 = document.getElementById("button2")
+
+btn1.addEventListener("mousedown",function(){
+    console.log("apertou o botão")
+})
+
+btn1.addEventListener("mouseup",function(){
+    console.log("soltou o botão")
+})
+
+btn2.addEventListener("dblclick",function(){
+    console.log("clicou duas vezes")
+})
+
+btn2.addEventListener("contextmenu",function(e){
+    e.preventDefault
+    console.log("Botão direito")
+})
+
+//Movimento do mouse
+//mousemove
+
+window.addEventListener("mousemove", function(e){ //imprime na tela os eixos x e y
+    console.log(e.x)
+    console.log(e.y)
+})
