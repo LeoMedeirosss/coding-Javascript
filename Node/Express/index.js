d6681ed68e51b3f348c86c0527f609322443a234
LeoMedeirosss <lmf@cesar.school>
2025-02-05 17:09:42 -0300

    Node - Express Postman

--- Node/Express/index.js
+++ Node/Express/index.js
@@ -5,6 +5,10 @@ app.get("/",function(req,res){
     res.send("First Express Route")
 }) //principal
 
+app.get("/test",function(req,res){
+    res.send("Testing Route")
+}) //principal
+
 app.listen(3000, function() {
     console.log("Aplicação funcinando na rota 3000")
 })
