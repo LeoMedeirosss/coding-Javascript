a1e313b99cef2cd88da2255659b95470891d8640
LeoMedeirosss <lmf@cesar.school>
2025-09-10 19:14:55 -0300

    curso 2 - exercicio 1

---
+++ Curso2/exercícios/assets/js/main.js
@@ -0,0 +1,62 @@
+//exercicio 1
+console.log("exercicio 1")
+array = [1,2,3,4,5];
+soma = array[0] + array[1] + array[2] + array[3] + array[4]
+console.log(soma)
+
+console.log(" ")
+//exercicio 2
+console.log("exercicio 2")
+
+
+console.log(" ")
+//exercicio 3
+console.log("exercicio 3")
+
+console.log(" ")
+//exercicio 4
+console.log("exercicio 4")
+
+console.log(" ")
+//exercicio 5
+console.log("exercicio 5")
+
+console.log(" ")
+//exercicio 6
+console.log("exercicio 6")
+
+console.log(" ")
+//exercicio 7
+console.log("exercicio 7")
+
+console.log(" ")
+//exercicio 8
+console.log("exercicio 8")
+
+console.log(" ")
+//exercicio 9
+console.log("exercicio 9")
+
+console.log(" ")
+//exercicio 10
+console.log("exercicio 10")
+
+console.log(" ")
+//exercicio 11
+console.log("exercicio 11")
+
+console.log(" ")
+//exercicio 12
+console.log("exercicio 12")
+
+console.log(" ")
+//exercicio 13
+console.log("exercicio 13")
+
+console.log(" ")
+//exercicio 14
+console.log("exercicio 14")
+
+console.log(" ")
+//exercicio 15
+console.log("exercicio 15")
